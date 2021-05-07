8c9e94190eca6c975b3e744d02718e7cdc4acdfc
KALASA-IVAN <nzakiramuhoza@gmail.com>
2021-05-07 07:09:21 +0200

    second commit

---
+++ header.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <title></title>
+</head>
+
+<body>
+    <header>
+        <nav class="nav_header_main">
+            <a class="header_logo" href="#">
+                <img src="./images/logo.png" alt="logo">
+            </a>
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="#">Portifolio</a></li>
+                <li><a href="#">About me</a></li>
+                <li><a href="#">Contact</a></li>
+            </ul>
+        </nav>
+        <div class="header_login">
+            <form action="./includes/login.inc.php" method="POST">
+                <input type="text" name="mailuid" placeholder="Username/E-mail...">
+                <input type="password" name="pwd" placeholder="Password...">
+                <button type="submit" name="login_submit">Login</button>
+            </form>
+            <a href="signup.php">Signup</a>
+            <form action="./includes/logout.inc.php" method="POST">
+                <button type="submit" name="logout_submit">Logout</button>
+            </form>
+        </div>
+    </header>
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+require "header.php";
+?>
+
+<main>
+<p>You are logged in!</p>
+<p>You are logged out!</p>
+</main>
+
+<?php
+require "footer.php";
+?>
