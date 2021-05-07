f9abcdc57ee242c5567084f2a274cc8b062abe0f
KALASA-IVAN <nzakiramuhoza@gmail.com>
2021-05-07 07:17:45 +0200

    modified

--- header.php
+++ header.php
@@ -15,23 +15,29 @@
             <a class="header_logo" href="#">
                 <img src="./images/logo.png" alt="logo">
             </a>
-            <ul>
-                <li><a href="index.php">Home</a></li>
-                <li><a href="#">Portifolio</a></li>
-                <li><a href="#">About me</a></li>
-                <li><a href="#">Contact</a></li>
-            </ul>
+            <div class="links">
+                <ul>
+                    <li><a href="index.php">Home</a></li>
+                    <li><a href="#">Portifolio</a></li>
+                    <li><a href="#">About me</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+            </div>
         </nav>
         <div class="header_login">
-            <form action="./includes/login.inc.php" method="POST">
-                <input type="text" name="mailuid" placeholder="Username/E-mail...">
-                <input type="password" name="pwd" placeholder="Password...">
-                <button type="submit" name="login_submit">Login</button>
-            </form>
-            <a href="signup.php">Signup</a>
-            <form action="./includes/logout.inc.php" method="POST">
-                <button type="submit" name="logout_submit">Logout</button>
-            </form>
+            <div class="inputs">
+                <form action="./includes/login.inc.php" method="POST">
+                    <input type="text" name="mailuid" placeholder="Username/E-mail...">
+                    <input type="password" name="pwd" placeholder="Password...">
+                    <button type="submit" name="login_submit">Login</button>
+                </form>
+            </div>
+            <div class="buttons">
+                <a href="signup.php">Signup</a>
+                <form action="./includes/logout.inc.php" method="POST">
+                    <button type="submit" name="logout_submit">Logout</button>
+                </form>
+            </div>
         </div>
     </header>
 </body>
